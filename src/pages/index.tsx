c76f87267607c80eab5e097837acdec18c8a83bc
56beats <sss346ab@gmail.com>
2025-05-07 01:45:12 +0900

    Categoryセクションを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from "next";
 import Layout from "@/components/Layout";
 import MainHeader from "@/components/presentational/MainHeader";
+import CategorySection from "@/components/presentational/CategorySection";
+import MemberSection from "@/components/presentational/MemberSection";
 
 const Page: NextPage = () => {
   return (
     <Layout>
       <MainHeader />
+      <CategorySection />
+      <MemberSection />
     </Layout>
   );
 };

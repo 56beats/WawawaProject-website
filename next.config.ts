d07f5de209dd4fb6d779e44a1d115c24ddcbceb5
sanshiro nishikawa <sss346ab@gmail.com>
2026-01-19 00:57:56 +0900

    バージョンアップ対応

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,3 @@
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig = {
-  assetPrefix: isProd ? "/WawawaProject-website/" : "",
-  basePath: isProd ? "/WawawaProject-website" : "",
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
